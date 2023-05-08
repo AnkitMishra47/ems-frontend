8fe01798addb0479f63ed2e2a174661971b698cd
Ankit Mishra <ankitm17.2001@gmail.com>
2023-05-09 00:06:20 +0530

    fixed environemt

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environment/environment';
-import { ConfirmationService, MessageService, ConfirmEventType } from 'primeng/api';
+import { MessageService} from 'primeng/api';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
